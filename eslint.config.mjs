1bd1c430e75630a5958106772368b795ed928200
Nazri Ghazi <nazrighazi02@gmail.com>
2024-08-03 13:51:34 +0800

    feat(repo): add pre commit checking and update prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,26 +5,26 @@ import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import globals from 'globals';
 import tseslint, { parser } from 'typescript-eslint';
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-    {
-        languageOptions: {
-            globals: globals.browser,
-            parser: parser,
-            parserOptions: { ecmaFeatures: { jsx: true } },
-        },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: parser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.stylistic,
-    pluginReact.configs.flat.recommended,
-    reactJsxRuntime,
-    {
-        rules: {
-            '@typescript-eslint/no-require-imports': 'warn',
-        },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  pluginReact.configs.flat.recommended,
+  reactJsxRuntime,
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'warn',
     },
-    eslintPluginPrettierRecommended,
-    {
-        ignores: ['scripts/', 'commitlint.config.js', 'babel.config.js', 'node_modules'],
-    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['scripts/', 'commitlint.config.js', 'babel.config.js', 'node_modules'],
+  },
 ];
